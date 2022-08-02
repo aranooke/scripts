80e4ab2ac2e60d6028fb5cfb11f5183e54a13abd
Artem <65169511+aranooke@users.noreply.github.com>
2022-08-02 11:00:21 +0300

    Second commit
    Commit is going

--- botTranslator.py
+++ botTranslator.py
@@ -1,7 +1,7 @@
 import logging
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
-
+from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
@@ -9,12 +9,15 @@ logging.basicConfig(
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token('5405306262:AAGJk9TKxDkMiN0gPvUyCXEx9rWatasHvB4').build()
     
     start_handler = CommandHandler('start', start)
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
     application.add_handler(start_handler)
-    
+    application.add_handler(echo_handler)
     application.run_polling()
 # 5405306262:AAGJk9TKxDkMiN0gPvUyCXEx9rWatasHvB4
